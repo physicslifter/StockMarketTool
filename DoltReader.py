11802981128cd1b86a208a945a6842c2a86f021d
Pat LaChapelle <31046873+physicslifter@users.noreply.github.com>
2026-01-05 08:14:04 -0500

    Added query for isolating data in get_data()

--- DoltReader.py
+++ DoltReader.py
@@ -5,6 +5,7 @@ the dolt stock market database from post-no-preference
 '''
 from mysql import connector as cnc
 import pandas as pd
+import numpy as np
 
 database_names = ["earnings", "options", "stocks"]
 report_types = ["quarterly", "yearly"]
@@ -13,9 +14,11 @@ class DataReader:
     def __init__(self):
         pass
 
-    def get_data(database_name, stock, start_date, end_date, report_type, table_names = None):
+    def get_data(self, database_name, stock, start_date:np.datetime64, end_date:np.datetime64, report_type:str, table_names:list = None):
         if database_name.lower() not in database_names:
             raise Exception(f"Invalid dataset name. Must be one of {database_names}")
+        if report_type not in ["Quarter", "Year"]:
+            raise Exception("Report type must be quarter or year")
         #connect to the dataset
         conn = cnc.connect(host = "127.0.0.1", 
                    port = 3306, 
@@ -33,7 +36,14 @@ class DataReader:
                 if name not in tables:
                     raise Exception(f"Table name {name} invalid for database {database_name}. Must be one of {tables}")
         
-        query = "SELECT * FROM"
-        query += ""
+        start_date_str, end_date_str = str(start_date).split(" ")[0], str(end_date).split(" ")[0] 
+        pandas_data = {}
+        for table_name in table_names:
+            print(table_name)
+            query = f"SELECT * FROM {table_name} WHERE act_symbol = '{stock}' AND date > '{start_date}' AND date < '{end_date}' AND period = '{report_type}';"
+            print(query)
+            df = pd.read_sql(query, conn)
+            pandas_data[table_name] = df
 
+        return pandas_data
     
