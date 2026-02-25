0b6646d40c0b69d465d5d46101d1cfd3f51d2a04
Pat LaChapelle <31046873+physicslifter@users.noreply.github.com>
2026-02-25 01:26:25 -0500

    Fixed save paths

--- DataRetrieval/setup_data.py
+++ DataRetrieval/setup_data.py
@@ -21,13 +21,13 @@ conn = cnc.connect(host = "127.0.0.1",
                    database = None)
 query = "SELECT * FROM stocks.ohlcv"
 all_data = pd.read_sql(query, conn)
-all_data.to_feather("Data/all_ohlcv.feather")
+all_data.to_feather("../Data/all_ohlcv.feather")
 
 etf_query = "SELECT * FROM stocks.symbol WHERE is_etf = '1';"
 etf_data = pd.read_sql(etf_query, conn)
-etf_data.to_feather("Data/ETFs.feather")
+etf_data.to_feather("../Data/ETFs.feather")
 
 #remove ETFs from the dataset
 ETFs = etf_data.act_symbol.unique()
 all_data = all_data[~all_data['act_symbol'].isin(ETFs)]
-all_data.to_feather("all_ohlcv_no_ETFs.feather")
+all_data.to_feather("../Data/all_ohlcv_no_ETFs.feather")
